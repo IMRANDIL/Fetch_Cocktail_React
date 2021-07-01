9aead0b2d35e3653753e3db1195a24ab6b0f8645
IMRANDIL <aliimranadil2@gmail.com>
2021-07-01 13:02:53 +0530

    Updates

---
+++ precache-manifest.e6f0723b2249ac846c9575e06d5cbcc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41caf97b354c05d7229e5aef3a4f203a",
+    "url": "/Fetch_Cocktail_React/index.html"
+  },
+  {
+    "revision": "5f59ff4b2676fa829ace",
+    "url": "/Fetch_Cocktail_React/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/Fetch_Cocktail_React/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Fetch_Cocktail_React/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f59ff4b2676fa829ace",
+    "url": "/Fetch_Cocktail_React/static/js/main.e0e02ebd.chunk.js"
+  },
+  {
+    "revision": "c438b260f277f78bf667",
+    "url": "/Fetch_Cocktail_React/static/js/runtime-main.b62ecba8.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/Fetch_Cocktail_React/static/media/logo.9a3d2645.svg"
+  }
+]);
