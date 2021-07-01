b46345a121f8298607f5525c62c2d92037175a9d
IMRANDIL <aliimranadil2@gmail.com>
2021-07-01 13:41:58 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as HashRouter, Route, Switch } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
@@ -9,7 +9,7 @@ import Error from './pages/Error'
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <Switch>
         <Route exact path='/'>
@@ -25,7 +25,7 @@ function App() {
           <Error />
         </Route>
       </Switch>
-    </Router>
+    </HashRouter>
   )
 }
 
