3f033f7bc6b9920db769c5a07d683615c673e726
IMRANDIL <aliimranadil2@gmail.com>
2021-07-01 12:50:09 +0530

    first commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext();
+  const searchValue = React.useRef('');
+
+React.useEffect(()=>{
+searchValue.current.focus()
+},[])
+
+const preventDefaultt = (e)=>{
+  e.preventDefault();
+}
+
+  const searchCocktail = ()=>{
+    const inputValue = searchValue.current.value;
+    setSearchTerm(inputValue)
+    if(!inputValue){
+      setSearchTerm('a')
+    }
+  }
+  
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={preventDefaultt}>
+        <div className="form-control">
+<label htmlFor="name">search your favorite cocktail :)</label>
+<input type="text" id='name' ref={searchValue} onChange={searchCocktail} />
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, corporis id! Ea commodi dignissimos consectetur asperiores provident facere nisi eveniet laborum labore maxime laudantium sit ab at iste quidem repudiandae, vitae atque quae cupiditate voluptate omnis dolorum? Culpa, maiores fugiat.</p>
+    </section>
+  )
+}
+
+export default About
